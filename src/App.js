db276d0e9b03fb6440c343c6aadd366e301e10b0
harshitc2015 <harshit@gnxtsystems.com>
2024-10-11 16:43:53 +0530

    4.43

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Home from './components/Home';
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      
+
+          <Routes>
+            <Route exact path="/signup" element={<Signup />}>
+              {" "}
+            </Route>
+            <Route exact path="/login" element={<Login />}>
+              {" "}
+            </Route>
+            <Route exact path="/" element={<Home/>}>
+              {" "}
+            </Route>
+            {/* <Route exact path="/about" element={<About />}>
+              {" "}
+            </Route>
+            <Route exact path="/notes" element={<Notes />}>
+              {" "}
+            </Route>
+            <Route exact path="/editnote" element={<EditNote />}>
+              {" "}
+            </Route>
+            <Route exact path="/addnote" element={<AddNote />}>
+              {" "}
+            </Route> */}
+          </Routes>
+        </Router>
+    </>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="container d-flex justify-content-center align-items-center">
+        <div class="btn btn1">Submit</div>
+      </div>
+
+      {/* <div class="btn btn2">Submit</div> */}
+    </>
+  );
+};
+
+export default Home;
